2688036a31390835b3ad0899d90067287a0ac952
Dustin Morrill <dmorrill10@gmail.com>
2015-07-27 01:53:40 +0000

    Refactor test

--- spec/mortar_spec.rb
+++ spec/mortar_spec.rb
@@ -19,9 +19,10 @@ def test_git_ssh_config(patient)
 end
 
 describe Mortar do
-  describe 'parses Brickfile.yml configuration with git https' do
-    it 'from string' do
-      test_brickfile_config = <<-END
+  describe 'parses Brickfile.yml configuration with' do
+    describe 'git https' do
+      it 'from string' do
+        test_brickfile_config = <<-END
 -
   name: mortar
   version: 0.0.1
@@ -29,43 +30,46 @@ describe Mortar do
     method: git
     url: https://github.com/dmorrill10/mortar.git
   END
-      test_git_https_config Mortar.parse(test_brickfile_config)
-    end
+        test_git_https_config Mortar.parse(test_brickfile_config)
+      end
 
-    it 'from file' do
-      test_git_https_config(
-        Mortar.parse_file(
-          File.expand_path(
-            '../support/test_files/git_https_brickfile.yml',
-            __FILE__
+      it 'from file' do
+        test_git_https_config(
+          Mortar.parse_file(
+            File.expand_path(
+              '../support/test_files/git_https_brickfile.yml',
+              __FILE__
+            )
           )
         )
-      )
-    end
-  end
-
-  describe 'parses Brickfile.yml configuration with git ssh' do
-    it 'from string' do
-      test_brickfile_config = <<-END
--
-  name: mortar
-  version: 0.0.1
-  location:
-    method: git
-    url: git@github.com:dmorrill10/mortar.git
-  END
-      test_git_ssh_config Mortar.parse(test_brickfile_config)
+      end
     end
 
-    it 'from file' do
-      test_git_ssh_config(
-        Mortar.parse_file(
-          File.expand_path(
-            '../support/test_files/git_ssh_brickfile.yml',
-            __FILE__
+    describe 'git ssh' do
+      it 'from string' do
+        test_brickfile_config = <<-END
+  -
+    name: mortar
+    version: 0.0.1
+    location:
+      method: git
+      url: git@github.com:dmorrill10/mortar.git
+    END
+        test_git_ssh_config Mortar.parse(test_brickfile_config)
+      end
+
+      it 'from file' do
+        test_git_ssh_config(
+          Mortar.parse_file(
+            File.expand_path(
+              '../support/test_files/git_ssh_brickfile.yml',
+              __FILE__
+            )
           )
         )
-      )
+      end
     end
+
+    # @todo Copy the above tests, but for svn, url, hg, and local project configurations (see support/test_files for example configurations)
   end
 end
