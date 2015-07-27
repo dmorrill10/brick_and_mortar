806a1376eb9278917342ae5d243d7bd48943545b
Dustin Morrill <dmorrill10@gmail.com>
2015-07-27 00:40:26 +0000

    Add YAML to configuration parsing for github ssh

--- spec/mortar_spec.rb
+++ spec/mortar_spec.rb
@@ -10,6 +10,14 @@ def test_git_https_config(patient)
   patient[0].location.url.must_equal 'https://github.com/dmorrill10/mortar.git'
 end
 
+def test_git_ssh_config(patient)
+  patient.length.must_equal 1
+  patient[0].name.must_equal 'mortar'
+  patient[0].version.must_equal '0.0.1'
+  patient[0].location.method.must_equal 'git'
+  patient[0].location.url.must_equal 'git@github.com:dmorrill10/mortar.git'
+end
+
 describe Mortar do
   describe 'parses Brickfile.yml configuration with git https' do
     it 'from string' do
@@ -29,6 +37,31 @@ describe Mortar do
         Mortar.parse_file(
           File.expand_path(
             '../support/test_files/git_https_brickfile.yml',
+            __FILE__
+          )
+        )
+      )
+    end
+  end
+
+  describe 'parses Brickfile.yml configuration with git ssh' do
+    it 'from string' do
+      test_brickfile_config = <<-END
+-
+  name: mortar
+  version: 0.0.1
+  location:
+    method: git
+    url: git@github.com:dmorrill10/mortar.git
+  END
+      test_git_ssh_config Mortar.parse(test_brickfile_config)
+    end
+
+    it 'from file' do
+      test_git_ssh_config(
+        Mortar.parse_file(
+          File.expand_path(
+            '../support/test_files/git_ssh_brickfile.yml',
             __FILE__
           )
         )
