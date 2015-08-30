354051b25afd362ddca1e2909556445fbb4663b4
Dustin Morrill <dmorrill10@gmail.com>
2015-08-29 23:27:57 -0600

    Fix tar.gz reader

--- Rakefile
+++ Rakefile
@@ -1,16 +1,18 @@
 require 'bundler/gem_tasks'
 require 'rake'
 require 'rake/clean'
-require 'rake/testtask'
 
 require_relative 'lib/mortar/version'
 
 desc 'Test gem'
 task default: [:test]
 
-Rake::TestTask.new do |t|
-  t.libs << 'lib' << 'spec/support'
-  t.test_files = FileList['spec/**/*_spec.rb']
-  t.verbose = false
-  t.warning = false
+task :test do
+  libs = ['lib', 'spec/support']
+  test_files = FileList['spec/**/*_spec.rb']
+
+  test_files.each do |test_file|
+    includes = libs.map { |l| "-I#{l}"}.join ' '
+    sh "ruby #{includes} #{test_file}"
+  end
 end
--- lib/mortar/download.rb
+++ lib/mortar/download.rb
@@ -5,7 +5,9 @@ require 'rubygems/package'
 
 module Mortar
   module Download
-    def self.name_from_url(url)
+    TAR_LONGLINK = '././@LongLink'
+
+    def self.name_from_path(url)
       File.basename(url, File.extname(url))
     end
     def self.unzip(zip_file)
@@ -29,59 +31,50 @@ module Mortar
     # Thanks to TomCZ
     # http://stackoverflow.com/questions/856891/unzip-zip-tar-tag-gz-files-with-ruby
     # for this
-    def self.deflate_tar_gz(path)
-      destination_to_return = nil
-      root_created = false
-      destination = '.'
-      Gem::Package::TarReader.new(Zlib::GzipReader.open(path)) do |tar|
+    def self.deflate_tar_gz(tar_gz_archive, destination = Dir.pwd)
+      first_directory = nil
+      Gem::Package::TarReader.new( Zlib::GzipReader.open tar_gz_archive ) do |tar|
         dest = nil
         tar.each do |entry|
-          if entry.full_name == path
+          if entry.full_name == TAR_LONGLINK
             dest = File.join destination, entry.read.strip
             next
           end
           dest ||= File.join destination, entry.full_name
-          if entry.directory? || (entry.header.typeflag == '' && entry.full_name.end_with?('/'))
-            File.delete dest if File.file? dest
+          if entry.directory?
+            FileUtils.rm_rf dest unless File.directory? dest
             FileUtils.mkdir_p dest, :mode => entry.header.mode, :verbose => false
-            unless root_created
-              destination_to_return = dest
+            first_directory = dest unless first_directory
+          elsif entry.file?
+            FileUtils.rm_rf dest unless File.file? dest
+            File.open dest, "wb" do |f|
+              f.print entry.read
             end
-            root_created = true
-          else
-            unless root_created
-              destination = name_from_url path
-              FileUtils.mkdir_p destination
-              destination_to_return = destination
-              root_created = true
+            FileUtils.chmod entry.header.mode, dest, :verbose => false
+            unless first_directory
+              puts "WARNING: File \"#{dest}\" created before a root directory"
             end
-            if entry.file? || (entry.header.typeflag == '' && !entry.full_name.end_with?('/'))
-              FileUtils.rm_rf dest if File.directory? dest
-              File.open dest, "wb" do |f|
-                f.print entry.read
-              end
-              FileUtils.chmod entry.header.mode, dest, :verbose => false
-            elsif entry.header.typeflag == '2' #Symlink!
-              File.symlink entry.header.linkname, dest
-            else
-              puts "WARNING: Unknown tar entry: #{entry.full_name} type: #{entry.header.typeflag}."
+          elsif entry.header.typeflag == '2' #Symlink!
+            File.symlink entry.header.linkname, dest
+            unless first_directory
+              puts "WARNING: Symlink \"#{dest}\" to \"#{entry.header.linkname}\" created before a root directory"
             end
           end
           dest = nil
         end
       end
-      destination_to_return
+      first_directory
     end
     def self.get_and_unpack_zip(url, name = nil, options = {})
       unless name
-        name = name_from_url url
+        name = name_from_path url
       end
       unzipped_dir_name = unzip(open(url))
       FileUtils.mv unzipped_dir_name, name
     end
     def self.get_and_unpack_tar_gz(url, name = nil, options = {})
       unless name
-        name = name_from_url url
+        name = name_from_path url
       end
       uncompressed_dir_name = deflate_tar_gz(open(url))
       FileUtils.mv uncompressed_dir_name, name
