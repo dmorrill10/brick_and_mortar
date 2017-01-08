e2c7cd5da7bf5ad1e9ebb510bb285b510357a940
Dustin Morrill <dmorrill10@gmail.com>
2017-01-08 08:45:40 -0700

    Fix git test

--- spec/git_spec.rb
+++ spec/git_spec.rb
@@ -1,20 +1,28 @@
 require_relative 'support/spec_helper'
+require 'git'
 
 require_relative '../lib/brick_and_mortar/git'
 
 describe BrickAndMortar::Git do
-  describe 'given git ssh brick configuration' do
-    git_ssh_brick_configruation = MockBrickConfiguration.new(
-      'mock_brick',
-      '0.0.1',
-      MockBrickLocation.new('git', 'git@github.com:dmorrill10/brick_and_mortar.git')
-    )
-
-    it 'downloads the brick with git ssh' do
+  describe 'given git brick configuration' do
+    it 'installs the brick with git' do
       Dir.mktmpdir do |dir|
-        Dir.chdir dir do |d|
-          BrickAndMortar::Git.clone_repo(git_ssh_brick_configruation.location.url, git_ssh_brick_configruation.name)
-          File.directory?(File.join(dir, git_ssh_brick_configruation.name)).must_equal true
+        mock_git_repo = "#{dir}/git_remotes/mock_git_repo.git"
+        FileUtils.mkdir_p mock_git_repo
+        Git.init mock_git_repo, bare: true
+        git_brick_configruation = MockBrickConfiguration.new(
+          'mock_brick',
+          '0.0.1',
+          MockBrickLocation.new('git', mock_git_repo)
+        )
+        Dir.chdir dir do |_d|
+          BrickAndMortar::Git.clone_repo(
+            git_brick_configruation.location.url,
+            git_brick_configruation.name
+          )
+          File.directory?(
+            File.join(dir, git_brick_configruation.name)
+          ).must_equal true
         end
       end
     end
@@ -23,14 +31,22 @@ describe BrickAndMortar::Git do
     git_https_brick_configruation = MockBrickConfiguration.new(
       'mock_brick',
       '0.0.1',
-      MockBrickLocation.new('git', 'https://github.com/dmorrill10/brick_and_mortar.git')
+      MockBrickLocation.new(
+        'git',
+        'https://github.com/dmorrill10/brick_and_mortar.git'
+      )
     )
 
     it 'downloads the brick with git https' do
       Dir.mktmpdir do |dir|
-        Dir.chdir dir do |d|
-          BrickAndMortar::Git.clone_repo(git_https_brick_configruation.location.url, git_https_brick_configruation.name)
-          File.directory?(File.join(dir, git_https_brick_configruation.name)).must_equal true
+        Dir.chdir dir do |_d|
+          BrickAndMortar::Git.clone_repo(
+            git_https_brick_configruation.location.url,
+            git_https_brick_configruation.name
+          )
+          File.directory?(
+            File.join(dir, git_https_brick_configruation.name)
+          ).must_equal true
         end
       end
     end
